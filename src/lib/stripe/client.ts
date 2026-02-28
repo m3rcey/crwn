04cf873397c7c7e02033747b08bd89fae78bc724
m3rcey <joshn.wms@gmail.com>
2026-02-27 21:20:12 -0600

    Fix: Update Stripe API version

--- src/lib/stripe/client.ts
+++ src/lib/stripe/client.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY!;
 
 export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2026-02-25.clover',
 });
 
 export const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY!;
