7cffd0918dd5beff47e3c408079371ba13c51c9c
m3rcey <joshn.wms@gmail.com>
2026-03-08 22:48:44 -0500

    fix: wrap artist profile tabs on mobile

--- src/components/artist/ArtistProfileContent.tsx
+++ src/components/artist/ArtistProfileContent.tsx
@@ -92,7 +92,7 @@ export function ArtistProfileContent({
     <>
       {/* Tabs */}
       <div className="px-4 sm:px-6 lg:px-8 mb-6">
-        <div className="flex gap-2">
+        <div className="flex flex-wrap gap-2">
           {tabs.map((tab) => (
             <button
               key={tab.id}
